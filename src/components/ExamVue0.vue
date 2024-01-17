edd10c86902a06fb18452d3c2889d1b674b739b0
yjchoi <yjchoi@strato.co.kr>
2024-01-17 13:31:55 +0900

    add

--- src/components/ExamVue0.vue
+++ src/components/ExamVue0.vue
@@ -5,8 +5,17 @@
  <p v-html="myhtml"></p>
  <p v-text="myArray[0]"></p>
  <p v-text="myTea[1].name"></p>
- <input v-model="myName">
- <p>나는 {{ myName }} 입니다.</p>
+ <p><input v-model="myName">나는 {{ myName }} 입니다.</p>
+ <p><input type="checkbox" v-model="myCheck"> 체크박스의 상태는 {{ myCheck }} </p>
+ <P>
+    <input type="radio" value="red"   v-model="picked"> 적
+    <input type="radio" value="green" v-model="picked"> 녹
+    <input type="radio" value="blue"  v-model="picked"> 청<br>
+    {{ picked }}이 선택됨
+ </P>
+ <p><button v-on:click="countUp">{{ count }}</button> </p>
+ <p> <input v-model="price" type="number">{{ taxIncluded }} </p>
+
 <hr> 
 
 
@@ -20,9 +29,9 @@
 <script>
 
 let teaList = [
-    	{name:'다즐링', price:600},
-        {name:'얼그레이',price:500},
-        {name:'실론',price:500}
+    {name   :'다즐링', price:600},
+    {name   :'얼그레이',price:500},
+    {name   :'실론',price:500}
     ]
 
 export default {
@@ -33,6 +42,25 @@ export default {
             myArray : ['다즐링','얼그레이','실론'],
             myTea : teaList,
             myName:"",
+            myCheck: true,
+            picked: "red",
+            count: 0,
+            price : 0,
+        }
+    },
+    methods: {
+        countUp: function(){
+            this.count++;
+        }
+    },
+    computed:{
+        taxIncluded : function () {
+            return this.price * 1.1;
+        }
+    },
+    watch:{
+        count : function(){
+            console.log("watch count value:"+this.count);
         }
     }
 }
