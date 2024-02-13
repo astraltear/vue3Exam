acbdcb7e783b2f5d1febdee2c8b44de92a6ae0c8
yjchoi <yjchoi@strato.co.kr>
2024-02-13 10:19:11 +0900

    add

---
+++ src/components/ExamVue21_Child.vue
@@ -0,0 +1,52 @@
+<template>
+  <ul>
+      <li v-on:click="countClicks">
+          <h2>{{ foodName }}</h2>
+          <p>{{ message }}</p>
+          <p v-show="isFavorite">{{ isFavorite }}</p>
+          <p id="red">You have clicked me {{ clicks }} times.</p>
+      </li>
+  </ul>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default{
+
+  props :{
+      foodName : String,
+      message : {type : String , required : true, default :  'This is the default description.' } ,
+      isFavorite : Boolean,
+  },
+
+/*   
+  위와 같이 객체 혹은 아래와 같이 배열로 선언할 수 있다.
+  
+ props: ["foodName", "message",'isFavorite'],
+*/
+  setup(props) {
+
+      //const foodName ="Apples";
+      //const message='I like apples'
+      const clicks= ref(0)
+
+      //return {foodName,message, clicks, countClicks }    
+      return { clicks, }    
+  },
+
+  methods: {
+      countClicks :  function (){
+          console.log(this.foodName, "countClicks")
+          this.clicks++;
+      }
+  },
+}
+</script>
+
+<style>
+
+#red {
+font-weight: bold ;
+color: rgb(144, 12, 12);
+}
+</style>
---
+++ src/components/ExamVue21_Parent.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- String과 다른 데이터 유형을 가진 props를 전달하려면 전달하려는 속성 앞에 v-bind:를 작성해야 합니다. -->
+  <ExamVue21_Child food-name="Apple" message="I like apples" v-bind:is-favorite="true"></ExamVue21_Child>  
+  <ExamVue21_Child foodName="Banana" message="I like bananas"  v-bind:is-favorite="false"></ExamVue21_Child>  
+  <ExamVue21_Child food-name="Pizza"/>
+  <ExamVue21_Child food-name="Rice"/>
+</template>
+
+<script>
+import ExamVue21_Child from './components/ExamVue21_Child.vue';
+
+export default{
+    setup(props) {
+        return {  };
+    },
+    components: { ExamVue21_Child }
+}
+</script>
